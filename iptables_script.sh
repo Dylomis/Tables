8f5df798ca4aaf96bb4b7b9cc8e469318be188ad
Dylomis <47687163+Dylomis@users.noreply.github.com>
2019-02-17 00:20:21 -0500

    Update iptables_script.sh

--- iptables_script.sh
+++ iptables_script.sh
@@ -18,8 +18,10 @@ iptables -A INPUT -p tcp -m tcp --dport 139 -m recent --name portscan --set -j D
 iptables -A FORWARD -p tcp -m tcp --dport 139 -m recent --name portscan --set -j LOG --log-prefix "portscan:"
 iptables -A FORWARD -p tcp -m tcp --dport 139 -m recent --name portscan --set -j DROP
 iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -s 172.20.240.20 --dport 3306 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 3306 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P INPUT DROP
@@ -27,7 +29,16 @@ service iptables save
 service iptables restart
 usermod -s /bin/false sysadmin
 usermod -L sysadmin
-yum update -y
-yum upgrade -y
 wget https://tinyurl.com/ydyujwnb
+tar xvzf ydyujwnb
+wget https://tinyurl.com/y8wbpkcl
+tar xvzf ydyujwnb
+wget https://tinyurl.com/y7yy5p9h
+tar xvzf y7yy5p9h
+cd maldetect-1.6.3 && ./install.sh
+cd .. 
+cd rkhunter-1.4.6 && ./installer.sh --install
+cd ..
+cd lynis && chown -R root:root *
+./lynis --check-all | more
 exit 0;
